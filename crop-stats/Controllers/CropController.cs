29abc2fa85a7950adfb2df5fd1ccc6a2dd6bbbf4
Örjan Sjöholm <orjansjoholm@gmail.com>
2013-03-19 21:08:39 +0100

    Added some basic angualr

---
+++ crop-stats/Controllers/CropController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crop_stats.Controllers
+{
+    public class CropController : Controller
+    {
+        //
+        // GET: /Crop/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ crop-stats/Scripts/app/crops.js
@@ -0,0 +1,20 @@
+﻿// declare a module
+var cropsApp = angular.module('cropsApp', []);
+
+// configure the module.
+// in this example we will create a greeting filter
+cropsApp.filter('greet', function () {
+    return function (name) {
+        return 'Hello, ' + name + '!';
+    };
+});
+
+cropsApp.controller('cropList', function ($scope) {
+    $scope.cropList = ["havre"];
+    $scope.apa = "Örjan";
+
+    $scope.addTodo = function () {
+        $scope.cropList.push($scope.cropType);
+    };
+
+});
